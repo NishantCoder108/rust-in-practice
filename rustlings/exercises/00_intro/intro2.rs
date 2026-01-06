7f025299228d92c0bf3a8bef9e6e92c21c2bc886
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-01-06 13:35:59 +0530

    exercise 0: work on printing stuff
    - how we can see logs using rust printing method

--- rustlings/exercises/00_intro/intro2.rs
+++ rustlings/exercises/00_intro/intro2.rs
@@ -1,4 +1,15 @@
 fn main() {
     // TODO: Fix the code to print "Hello world!".
-    printline!("Hello world!");
+    println!("Hello world by println!");
+    print!("Hello world by print!");
+    eprint!("Welcome of Rust error world!");
+
+    println!("{0:_>35}, {1:_<35}", "Welcome to your Dream World!", "_");
+
+    println!("{0:_^45}", "Both side equal dash(-)")
+
+    /*
+    This is multiline comment. It will just wrap up
+
+     */
 }

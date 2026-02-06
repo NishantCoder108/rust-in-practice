85bc608538c3773d08a97a7baa3be9b711c4ace2
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-02-06 09:57:38 +0530

    Enhance Hello World program initialization with data handling
    This commit modifies the `initialize` function to accept a `u64` data parameter, allowing the program to store this data in a new account. Additionally, the `Initialize` struct is updated to include necessary account specifications for the new account, improving the program's functionality and account management.

--- rektoff/module2/07_solana_fundaments/hello-world/programs/hello-world/src/lib.rs
+++ rektoff/module2/07_solana_fundaments/hello-world/programs/hello-world/src/lib.rs
@@ -6,11 +6,34 @@ declare_id!("GCagCXvTjjcH4LrmPoCnVAypBq2yhnnU3McSF8sQFUqC");
 pub mod hello_world {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        msg!("Changed data to: {}!", data);
+        ctx.accounts.new_account.data = data;
         Ok(())
     }
 }
 
+// #[derive(Accounts)]
+// pub struct Initialize {}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    // We must specify the space in order to initialize an account.
+    // First 8 bytes are default account discriminator,
+    // next 8 bytes come from NewAccount.data being type u64.
+    // (u64 = 64 bits unsigned integer = 8 bytes)
+    #[account(init, payer = signer, space = 8 + 8)]
+    pub new_account: Account<'info, NewAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+// #[derive(InitSpace)]
+pub struct NewAccount {
+    data: u64,
+    // #[max_len(30)]
+    // name: String,
+}

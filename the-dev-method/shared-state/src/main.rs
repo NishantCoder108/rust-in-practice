51e7f85955edb7464ef42a216f0565bc5cd0a16d
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-02-12 23:53:07 +0530

    Update main.rs to modify Mutex usage example and enhance documentation
    This commit updates the `main.rs` file to replace the existing Mutex variable with a new one, demonstrating a multiplication operation on the locked data. Additionally, it improves the comments to clarify the Mutex locking mechanism and its implications, including details about the 'poisoned' state. These changes aim to provide a clearer understanding of Mutex functionality in a single-threaded context.

--- the-dev-method/shared-state/src/main.rs
+++ the-dev-method/shared-state/src/main.rs
@@ -2,12 +2,24 @@ use std::sync::Mutex;
 
 /* Using Mutex - in single threaded context */
 fn main() {
-    let m = Mutex::new(5);
+    let n = Mutex::new(9);
 
     {
-        let mut num = m.lock().unwrap();
-        *num = 6;
-    }
+        let mut num = n.lock().unwrap();
+        *num *= 12;
+        // The lock is automatically released (unlocked) here, when 'num' goes out of scope.
+    } // <-- unlock happens here
 
-    println!("m = {m:?}");
+    // When printing a Mutex with Debug, you'll see fields like 'data' and 'poisoned'.
+    // 'poisoned' indicates if a panic occurred while the Mutex was locked. If a thread panics while holding the lock,
+    // the Mutex is considered poisoned to signal possible data inconsistency. Here, 'poisoned: false' means no panic occurred.
+    println!("n = {n:?}"); // e.g., Mutex { data: 108, poisoned: false, .. }
+
+    /* Understand with steps:
+    Mutex : locks the data -> do stuff using data -> If done with data -> unlock the data
+    1. first we acquire the lock. i.e. `m.lock()`
+    2. now we can access the data and mutate it
+    3. unlock the data if it requires.
+
+    */
 }

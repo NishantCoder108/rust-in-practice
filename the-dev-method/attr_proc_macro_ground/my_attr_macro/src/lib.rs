94294f5830c236728fa246b77e3b5a6fa1c4c70c
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-02-25 00:14:59 +0530

    Add workspace structure with app and my_attr_macro packages
    This commit establishes a new workspace for the `attr_proc_macro_ground` project, introducing two packages: `app` and `my_attr_macro`. The `app` package includes a basic `main.rs` that prints "Hello, world!", while the `my_attr_macro` package implements a procedural macro `log_call`. The `Cargo.toml` and `Cargo.lock` files are created to manage dependencies and workspace configuration.

---
+++ the-dev-method/attr_proc_macro_ground/my_attr_macro/src/lib.rs
@@ -0,0 +1,20 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse_macro_input;
+
+#[proc_macro_attribute]
+pub fn log_call(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemFn);
+
+    let name = &input.sig.ident;
+    let block = &input.block;
+
+    let expanded = quote! {
+        fn #name() {
+            println!("Function {} called!", stringify!(#name));
+            #block
+        }
+    };
+
+    TokenStream::from(expanded)
+}

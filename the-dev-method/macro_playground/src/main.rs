6f2d3bc47be1ac2479982531f82165b3c726f7e2
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-02-24 14:37:01 +0530

    macros: declarative macro implementation, repetitive pattern, in main.rs
    This commit reintroduces the `my_vec` macro in `main.rs`, allowing for the creation of a vector from a variable number of expressions. The previous `hi_macro` has been commented out, and the main function now demonstrates the usage of `my_vec` to create and print a vector. This change enhances the macro playground by showcasing a new macro functionality.

--- the-dev-method/macro_playground/src/main.rs
+++ the-dev-method/macro_playground/src/main.rs
@@ -13,33 +13,33 @@
 //     say_hello!();
 // }
 
-// macro_rules! my_vec {
-//     ( $(  ),* ) => {
-//         {
-//             let mut temp = Vec::new();
-//             $(
-//                 temp.push($x);
-//             )*
-//             temp
-//         }
-//     };
-// }
-
-// fn main() {
-//     let v = my_vec![1, 2, 3];
-//     println!("{:?}", v);
-// }
-
-macro_rules! hi_macro {
-    () => {
-        println!("Hi");
-    };
-    ($msg:expr) => {
-        println!("Hi, {}", $msg);
+macro_rules! my_vec {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp = Vec::new();
+            $(
+                temp.push($x);
+            )*
+            temp
+        }
     };
 }
 
 fn main() {
-    hi_macro!();
-    hi_macro!("Nishant");
+    let v = my_vec![1, 2, 3];
+    println!("{:?}", v);
 }
+
+// macro_rules! hi_macro {
+//     () => {
+//         println!("Hi");
+//     };
+//     ($msg:expr) => {
+//         println!("Hi, {}", $msg);
+//     };
+// }
+
+// fn main() {
+//     hi_macro!();
+//     hi_macro!("Nishant");
+// }

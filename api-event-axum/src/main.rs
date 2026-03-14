62a59c62af0b68354c9fad9b820f38148f4fd1dd
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-03-15 01:14:00 +0530

    setup initial files for api-event-axum project

---
+++ api-event-axum/src/main.rs
@@ -0,0 +1,20 @@
+use axum::{
+    Router,
+    routing::{get, post},
+};
+
+
+#[tokio::main]
+async fn main() {
+    //Create route
+    let router01 = Router::new()
+        .route("/", get(vehicle_get))
+        .route("/", post(vehicle_post));
+
+    //ip address  and listen the port
+    let address = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+
+    //serve the port and runnning the application
+    axum::serve(listener, router01).await.unwrap();
+}

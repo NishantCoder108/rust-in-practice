2e9f146a7fff5338e5dbc5f511b6d92c86923e3f
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-03-16 17:16:05 +0530

    Add c02 xadd example for Redis streams

---
+++ rust-xp-valkey-stream/examples/c02-xadd.rs
@@ -0,0 +1,37 @@
+use redis::streams::{StreamMaxlen, StreamReadOptions, StreamReadReply};
+use redis::{Client, Commands};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = redis::Client::open("redis://127.0.0.1:6379")?;
+
+    let mut con = client.get_connection()?;
+
+    let stream_name = "stream-c02";
+
+    // -- Add an entry
+    // * -> means it will generate autmatically id
+    let id: String = con.xadd(stream_name, "*", &[("name", "Jen"), ("surname", "Donavan")])?;
+    println!("XADD - id: {id}");
+
+    // -- Read
+    let res: StreamReadReply = con.xread(&[stream_name], &["0"])?;
+    println!("Entries:\n{res:#?}");
+
+    // -- Read only one
+    let options = StreamReadOptions::default().count(1);
+    let res: StreamReadReply = con.xread_options(&[stream_name], &["0"], &options)?;
+    println!("Single Entry:\n{res:#?}");
+
+    // -- XTrim
+    // Delete the entries
+    let res: u64 = con.xtrim(stream_name, StreamMaxlen::Equals(0))?;
+    println!("xtrim count: {res}");
+
+    // -- Del
+    // Delete the key
+    let res: u64 = con.del(stream_name)?;
+    println!("del count: {res}"); // always be 1 (from above)
+
+    Ok(())
+}

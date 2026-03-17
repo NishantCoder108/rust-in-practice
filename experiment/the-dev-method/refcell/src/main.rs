ffcc9ff6bea668509e08b7924df764a3fa19d5e8
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-03-17 13:35:23 +0530

    Add Rc<RefCell> based List example

--- experiment/the-dev-method/refcell/src/main.rs
+++ experiment/the-dev-method/refcell/src/main.rs
@@ -1,4 +1,5 @@
-use std::cell::{Ref, RefCell};
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
 
 // fn main() {
 //     // let x = RefCell::new(5); // x is NOT mutable
@@ -15,9 +16,31 @@ use std::cell::{Ref, RefCell};
 
 // }
 
-fn main() {
-    let x = RefCell::new(10);
+// fn main() {
+//     let x = RefCell::new(10);
 
-    let a = x.borrow();
-    let b = x.borrow_mut(); // ❌ runtime panic
+//     let a = x.borrow();
+//     let b = x.borrow_mut(); // ❌ runtime panic
+// }
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use std::rc::Rc;
+
+fn main() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
 }

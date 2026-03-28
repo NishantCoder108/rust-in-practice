5af8dd01b3663bc522aa75a242c74b3e96778924
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-03-28 14:06:06 +0530

    Instead of passing func as a arg , we can use `move` keyword to access the local variable

--- concurrency/basic/src/main.rs
+++ concurrency/basic/src/main.rs
@@ -1,35 +1,48 @@
 use std::thread;
 
 fn main() {
-    let t1 = thread::spawn(f);
-    let t2 = thread::spawn(f);
+    let name: &'static str = "nishant";
+    let age = 25;
+    let message = "Hello from main thread".to_string();
 
-    let main_thread_id = thread::current().id();
-    println!("Hello from the main thread: {main_thread_id:?}");
-    // thread::sleep(std::time::Duration::from_millis(100));
 
-    let numbers = Vec::from_iter(0..=1000);
-
-    let t = thread::spawn(move || { //using move keyword to transfer ownership of numbers or any variable to the thread
-        let len = numbers.len();
-        let sum = numbers.iter().sum::<usize>();
-        sum / len
-    });
-
-    let average = t.join().unwrap();
-
-    println!("average: {average}");
-
-    t1.join().unwrap();
-    t2.join().unwrap(); //It will wait for the thread to finish
+    println!("Name: {name}, Age: {age}");
+    thread::spawn(move || { //when we need to move the variable to the thread, we need to use `move` keyword
+        println!("Hello from another thread: {message}");
+    }).join().unwrap();
+     
 }
 
-fn f() {
-    println!("Hello from another thread!");
+// fn main() {
+//     let t1 = thread::spawn(f);
+//     let t2 = thread::spawn(f);
 
-    let thread = thread::current();
-    let id = &thread.id();
-    let thread_name = thread.name();
-    println!("This is my thread id: {id:?}");
-    println!("This is my thread name: {thread_name:?}");
-}
+//     let main_thread_id = thread::current().id();
+//     println!("Hello from the main thread: {main_thread_id:?}");
+//     // thread::sleep(std::time::Duration::from_millis(100));
+
+//     let numbers = Vec::from_iter(0..=1000);
+
+//     let t = thread::spawn(move || { //using move keyword to transfer ownership of numbers or any variable to the thread
+//         let len = numbers.len();
+//         let sum = numbers.iter().sum::<usize>();
+//         sum / len
+//     });
+
+//     let average = t.join().unwrap();
+
+//     println!("average: {average}");
+
+//     t1.join().unwrap();
+//     t2.join().unwrap(); //It will wait for the thread to finish
+// }
+
+// fn f() {
+//     println!("Hello from another thread!");
+
+//     let thread = thread::current();
+//     let id = &thread.id();
+//     let thread_name = thread.name();
+//     println!("This is my thread id: {id:?}");
+//     println!("This is my thread name: {thread_name:?}");
+// }

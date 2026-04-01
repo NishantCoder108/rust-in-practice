050e6e73d5b8608e886fafb8913f10ca356b3406
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-04-01 16:45:53 +0530

    conc: we can store any types such as `struct`, `enum` or inbuilt types inside
    Add examples of stack-allocated arrays and multiple vector types in main.rs. Introduce custom struct and enum for demonstration. Update comments for clarity on vector usage.

--- collections/basic/src/main.rs
+++ collections/basic/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    let arr = [1, 2, 3, 3, 4, 4]; //store inside the stack instead of heap
     let mut vec: Vec<i32> = Vec::new();
 
     println!("{vec:?}");
@@ -9,13 +10,52 @@ fn main() {
 
     println!("Vec: {vec:?}");
 
+    let mut v2 = vec![];
+
+    println!("{v2:?}");
+
+    v2.push(3);
     // let mut vec = vec![];
 
     // vec.push(4);
     // assert_eq!(4, vec[0]);
     // println!("Vec: {vec:?}");
+
+    let mut v3 = vec![];
+
+    println!("{v3:?}");
+
+    let vect1 = VecT {
+        id: 1,
+        name: "Nishant".to_string(),
+    };
+
+    v3.push(vect1);
+
+    println!("{v3:?}");
+
+    let mut v4 = Vec::new();
+
+    println!("{v4:?}");
+
+    let e1 = ENUM::TECT;
+
+    v4.push(e1);
+
+    println!("{v4:?}");
 }
 
+#[derive(Debug)]
+struct VecT {
+    id: u32,
+    name: String,
+}
+
+#[derive(Debug)]
+enum ENUM {
+    VEC,
+    TECT,
+}
 /*
 Collections:
 1. Vectors - stores data that can increase at runtime
@@ -35,5 +75,13 @@ Vectors:
 2. Updating: -
  - push(value)
  - required `mut` keyword
+
+
+ Vec<T> :- T is types, so what types will be inside, here we can define
+ e.g. Vec<i32> :- i32 types will be inside this vector. vector is nothing but it just store dynamic elements that can increase or decrease at runtime.
+
+3. Accessing:-
+ -
+
 
 */

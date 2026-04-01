be353c75bd03425748352eca36e736831ca1c6d0
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-04-01 16:18:05 +0530

    conc: for creating, we can use two methods: 1. vec! 2. Vec::new()
    Add .gitignore and initialize Cargo.lock; update main.rs with vector examples and comments on collections

--- collections/basic/src/main.rs
+++ collections/basic/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let mut vec: Vec<i32> = Vec::new();
+
+    println!("{vec:?}");
+
+    //for update
+
+    // let mut vec = vec![];
+
+    // vec.push(4);
+    // assert_eq!(4, vec[0]);
+    // println!("Vec: {vec:?}");
 }
+
+/*
+Collections:
+1. Vectors - stores data that can increase at runtime
+2. Hashmap - store as key value pairs
+3. Strings - collections of characters
+
+
+Vectors:
+
+1. Creations: -
+ - vec![]
+ - Vec::new();
+
+ //For creating variable or defining some value so for that we can use `vec![]` or `Vec::new()`
+
+*/

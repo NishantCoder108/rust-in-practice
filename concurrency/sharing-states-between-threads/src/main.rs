6c650230924479bcfa63c3e782db5f34637fa6f0
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-04-08 23:03:16 +0530

    Refactor main.rs to implement thread-safe counter using Arc and Mutex
    Enhance the example of sharing state between threads by introducing an Arc-wrapped Mutex for a counter. This update demonstrates concurrent incrementing of a shared value across multiple threads, improving the overall concurrency model in the application.

--- concurrency/sharing-states-between-threads/src/main.rs
+++ concurrency/sharing-states-between-threads/src/main.rs
@@ -1,16 +1,40 @@
-use std::sync::Mutex;
+use std::{
+    sync::{Arc, Mutex},
+    thread,
+};
 
 fn main() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handlers = vec![];
+
+    for _ in 0..10 {
+        let data = Arc::clone(&counter);
+
+        let handler = thread::spawn(move || {
+            let mut counter = data.lock().unwrap();
+            *counter += 1;
+        });
+
+        handlers.push(handler);
+    }
+
+    for handler in handlers {
+        handler.join().unwrap();
+    }
+
+    println!("Counter : {:?}", *counter.lock().unwrap());
     // println!("Hello, world!");
 
-    let data = Mutex::new(0);
+    // let data = Mutex::new(0);
 
-    {
-        let mut lock_data = data.lock().unwrap();
+    // {
+    //     let mut lock_data = data.lock().unwrap();
 
-        println!("LOCK DATA: {lock_data:?}");
+    //     println!("LOCK DATA: {lock_data:?}");
 
-        *lock_data += 1;
-    }
-    println!("Data: {data:?}");
+    //     *lock_data += 1;
+    // }
+    // println!("Data: {data:?}");
 }
+
+//when goes out of scope then automatically lock value end

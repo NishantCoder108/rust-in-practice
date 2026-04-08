d0433b8551394e99682805be41ab5d719683a359
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-04-08 20:18:20 +0530

    Enhance message passing between threads in main.rs
    Implement additional functionality to send messages from a spawned thread to the main thread using Rust's mpsc channel. This update includes sending multiple messages and receiving them in the main thread, demonstrating effective communication between threads.

--- concurrency/passing-message-between-threads/src/main.rs
+++ concurrency/passing-message-between-threads/src/main.rs
@@ -3,6 +3,26 @@ use std::sync::mpsc;
 use std::thread;
 
 fn main() {
+    let (tx, receiver) = mpsc::channel::<String>();
+
+    let send_res = tx.send("Sending data between threads".to_string()).unwrap();
+
+    // match send_res {
+    //     Ok(v) => println!("Sending data ..., {:#?}", v),
+    //     Err(e) => println!("Not able to sending...., Err: {}", e),
+    // }
+
+    thread::spawn(move || {
+        tx.send("Sending data from separate threads".to_string())
+            .unwrap();
+        tx.send("Another sending data from separate threads".to_string())
+            .unwrap();
+        println!("Sent message from thread.");
+    });
+
+    let message = receiver.recv().unwrap();
+    println!("Main thread got data : {}", message);
+
     // let handler = thread::spawn(|| {
     //     println!("Handler");
     // })
@@ -14,23 +34,23 @@ fn main() {
 
     // println!("VEC: {vec:?}");
 
-    let (tx, rx) = mpsc::channel::<u32>();
+    // let (tx, rx) = mpsc::channel::<u32>();
 
-    let process_instruction = move || {
-        let tx_clone = tx.clone();
-        tx_clone.send(100).unwrap();
-        tx_clone.send(200).unwrap();
-        tx.send(300).unwrap();
-    };
+    // let process_instruction = move || {
+    //     let tx_clone = tx.clone();
+    //     tx_clone.send(100).unwrap();
+    //     tx_clone.send(200).unwrap();
+    //     tx.send(300).unwrap();
+    // };
 
-    let handler = thread::spawn(process_instruction);
+    // let handler = thread::spawn(process_instruction);
 
-    // handler.join().unwrap();
-    // drop(tx);
-    for re in rx {
-        // let rv_res = re.recv().unwrap();
-        println!("Receiver result: {re:?}");
-    }
+    // // handler.join().unwrap();
+    // // drop(tx);
+    // for re in rx {
+    //     // let rv_res = re.recv().unwrap();
+    //     println!("Receiver result: {re:?}");
+    // }
 }
 
 //Sending data between threads

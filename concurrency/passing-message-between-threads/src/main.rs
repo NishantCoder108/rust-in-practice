af5bc9bf06a2ddeaaf6fbb587ce27ec2b0bb608c
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-04-08 18:39:54 +0530

    Add message passing functionality between threads
    Implement a basic example in main.rs that demonstrates sending messages between threads using Rust's mpsc (multiple producer, single consumer) channel. This update includes the creation of a new Cargo.lock file and sets the stage for further exploration of concurrency in Rust.

--- concurrency/passing-message-between-threads/src/main.rs
+++ concurrency/passing-message-between-threads/src/main.rs
@@ -1,3 +1,37 @@
+use std::sync::mpsc;
+// use std::sync::mpsc::Sender;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    // let handler = thread::spawn(|| {
+    //     println!("Handler");
+    // })
+    // .join()
+    // .unwrap();
+
+    // let num = [1, 2, 3, 3, 4, 5];
+    // let vec = Vec::from(&num[0..6]);
+
+    // println!("VEC: {vec:?}");
+
+    let (tx, rx) = mpsc::channel::<u32>();
+
+    let process_instruction = move || {
+        let tx_clone = tx.clone();
+        tx_clone.send(100).unwrap();
+        tx_clone.send(200).unwrap();
+        tx.send(300).unwrap();
+    };
+
+    let handler = thread::spawn(process_instruction);
+
+    // handler.join().unwrap();
+    // drop(tx);
+    for re in rx {
+        // let rv_res = re.recv().unwrap();
+        println!("Receiver result: {re:?}");
+    }
 }
+
+//Sending data between threads
+// We can use - MPSC (multiple producer Single Consumer)

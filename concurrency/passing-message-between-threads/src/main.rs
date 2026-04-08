6d6365eba6b56260e23605e2f858803ee4678a98
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-04-08 20:57:33 +0530

    Refactor thread message sending in main.rs
    Enhance the message sending logic by spawning multiple threads that send formatted messages to the main thread. This update improves the demonstration of concurrent message passing and includes a mechanism to receive messages with a timeout, ensuring efficient communication between threads.

--- concurrency/passing-message-between-threads/src/main.rs
+++ concurrency/passing-message-between-threads/src/main.rs
@@ -1,28 +1,53 @@
 use std::sync::mpsc;
+use std::time::Duration;
 // use std::sync::mpsc::Sender;
-use std::thread;
+use std::{thread, time};
 
 fn main() {
     let (tx, receiver) = mpsc::channel::<String>();
 
-    let send_res = tx.send("Sending data between threads".to_string()).unwrap();
+    for thread_id in 0..10 {
+        println!("Thread Id: {thread_id:?}");
 
-    // match send_res {
-    //     Ok(v) => println!("Sending data ..., {:#?}", v),
-    //     Err(e) => println!("Not able to sending...., Err: {}", e),
+        let tx_clone = tx.clone();
+
+        thread::spawn(move || {
+            for i in 0..10 {
+                let mess = format!("Id {i}: spawn message sending at {thread_id}");
+                tx_clone.clone().send(mess).unwrap();
+            }
+        });
+    }
+
+    println!("REC: {:?}", receiver.recv().unwrap());
+
+    drop(tx); //it is important to drop `tx`, otherwise it will wait till all execution complete
+    // for res in receiver {
+    //     println!("RES: {res:?}");
     // }
 
-    thread::spawn(move || {
-        tx.send("Sending data from separate threads".to_string())
-            .unwrap();
-        tx.send("Another sending data from separate threads".to_string())
-            .unwrap();
-        println!("Sent message from thread.");
-    });
-
-    for res_data in receiver {
-        println!("Received message : {res_data:?}");
+    while let Ok(res) = receiver.recv_timeout(Duration::from_secs(1)) {
+        println!("RES: {res:?}");
     }
+    // thread::sleep(Duration::from_secs(2));
+    // let send_res = tx.send("Sending data between threads".to_string()).unwrap();
+
+    // // match send_res {
+    // //     Ok(v) => println!("Sending data ..., {:#?}", v),
+    // //     Err(e) => println!("Not able to sending...., Err: {}", e),
+    // // }
+
+    // thread::spawn(move || {
+    //     tx.send("Sending data from separate threads".to_string())
+    //         .unwrap();
+    //     tx.send("Another sending data from separate threads".to_string())
+    //         .unwrap();
+    //     println!("Sent message from thread.");
+    // });
+
+    // for res_data in receiver {
+    //     println!("Received message : {res_data:?}");
+    // }
     // while let Ok(res) = receiver.recv() {
     //     println!("Received Mess: {res:?}");
     // }
@@ -61,3 +86,5 @@ fn main() {
 
 //Sending data between threads
 // We can use - MPSC (multiple producer Single Consumer)
+
+//for checking number of cpu: sysctl -n hw.ncpu

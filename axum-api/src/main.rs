8985cacecfaaff9509a54e778e2b364b5508efbf
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-04-28 15:40:09 +0530

    Nested routes in axum
    - Introduced new routes for retrieving, updating, and deleting users.
    - Added a route for serving files and a catch-all route for handling not found errors.
    - Updated the main function to organize user-related routes under a versioned API structure.

--- axum-api/src/main.rs
+++ axum-api/src/main.rs
@@ -26,15 +26,45 @@ async fn list_users() -> &'static str {
 async fn create_user() -> &'static str {
     "Create User"
 }
+
+async fn get_user() -> &'static str {
+    "Get User by id"
+}
+
+async fn serve_file() -> &'static str {
+    "Serve file by path"
+}
+
+async fn not_found() -> &'static str {
+    "Not found"
+}
+
+async fn update_user() -> &'static str {
+    "Update user"
+}
+
+async fn delete_user() -> &'static str {
+    "Delete user"
+}
 #[tokio::main]
 async fn main() {
-    // let g = || async { "Hellow, World!" };
+    let user_routes = Router::new()
+        .route("/", get(list_users).post(create_user))
+        .route("/{id}", get(get_user).put(update_user).delete(delete_user));
+
     let app = Router::new()
-        .route("/", get(index))
-        .route("/hello", get(hello))
-        .route("/about", get(about))
-        .route("/signup", post(signup))
-        .route("/users", get(list_users).post(create_user));
+        .nest("/v1/api/users", user_routes)
+        .route("/{*path}", get(not_found));
+    // let g = || async { "Hellow, World!" };
+    // let app = Router::new()
+    //     .route("/", get(index))
+    //     .route("/hello", get(hello))
+    //     .route("/about", get(about))
+    //     .route("/signup", post(signup))
+    //     .route("/users", get(list_users).post(create_user))
+    //     .route("/users/{id}", get(get_user))
+    //     .route("/files/{*path}", get(serve_file))
+    //     .route("/{*notfound}", get(not_found));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
@@ -53,4 +83,9 @@ async fn main() {
 6. Middleware with Tower
 7. Database Integration - sqlx
 8. Serving Static files
+
+
+
+
+/user/1
 */

c0a40daa1ba02ade99a2c83f88ebe0e5b74cd372
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-04-28 14:17:29 +0530

    Add axum API setup with initial routing and dependencies
    - Introduced a .gitignore file to exclude target directory.
    - Created a Cargo.lock file to manage dependencies.
    - Updated Cargo.toml to include axum and tokio dependencies.
    - Implemented a basic main.rs with a simple "hello world" route using axum.
    - Added comments outlining future enhancements for the API.

--- axum-api/src/main.rs
+++ axum-api/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Router,
+    routing::{delete, get, post},
+};
+
+async fn hello() -> String {
+    return "hello world".to_string();
 }
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hellow, World!" }));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Listening on http://localhost:3000");
+    axum::serve(listener, app).await.unwrap();
+}
+
+/*
+
+1. Routing
+2. Handler
+3. Extractors - Path, Query, Json, Form
+4. Application State
+
+5. Error Handling
+6. Middleware with Tower
+7. Database Integration - sqlx
+8. Serving Static files
+*/

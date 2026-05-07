b670eb63ca0a3a393845f71986a776cf54d06e0d
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-05-07 22:35:08 +0530

    Implement PostgreSQL connection in main function
    - Updated the main function to establish a connection to a PostgreSQL database using sqlx and tokio for asynchronous execution.
    - Enhanced error handling by allowing the function to return any error type that implements the Error trait.
    - Added comments outlining the setup steps for running the PostgreSQL database locally.

--- postgres/bookstore/src/main.rs
+++ postgres/bookstore/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use sqlx::Connection;
+
+#[tokio::main]
+// The `Box<dyn Error>` type allows the function to return any error type that implements the `Error` trait.
+// This enables flexible error handling by boxing different kinds of errors into a single return type.
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "postgres::/dbuser:mysecretpassword@localhost:5432/bookstore";
+    let mut conn = sqlx::postgres::PgConnection::connect(url).await?;
+
+    Ok(())
 }
+
+/*
+1. Just setup bookstore app
+2. connnect with database
+3. Make sure function return Result
+4. Run this command for running postgres at locally
+
+  $ docker run -e POSTGRES_PASSWORD=mysecretpassword -e POSTGRES_USER=dbuser -e POSTGRES_DB=bookstore  -p 5432:5432 postgres
+  database  username  password  Host   Port  dbname
+
+
+5. Run query to test connection db
+
+5.
+*/

577c11b88fd305bed6924ec6e71d02db19d3d111
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-05-08 07:44:54 +0530

    Add book struct and create function for database interaction
    - Introduced a `Book` struct to represent book data with fields for title, author, and ISBN.
    - Implemented an asynchronous `create` function to insert new book records into the database.
    - Updated the main function to prepare a sample book instance, demonstrating how to use the new `create` function.
    - Commented out previous queries to focus on the new functionality and added guidance for future database interactions.

--- postgres/bookstore/src/main.rs
+++ postgres/bookstore/src/main.rs
@@ -2,6 +2,25 @@ use std::error::Error;
 
 use sqlx::{Connection, Row, types::Text};
 
+struct Book {
+    pub title: String,
+    pub author: String,
+    pub isbn: String,
+}
+
+async fn create(book: &Book, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
+    let query = "INSERT INTO book (title, author, isbn) VALUES ($1, $2, $3)";
+
+    sqlx::query(query)
+        .bind(&book.title)
+        .bind(&book.author)
+        .bind(&book.isbn)
+        .execute(pool)
+        .await?;
+
+    Ok(())
+}
+
 #[tokio::main]
 // The `Box<dyn Error>` type allows the function to return any error type that implements the `Error` trait.
 // This enables flexible error handling by boxing different kinds of errors into a single return type.
@@ -11,16 +30,23 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let pool = sqlx::PgPool::connect(url).await?;
     sqlx::migrate!("./migrations").run(&pool).await?; //add migration to to do
 
-    let res = sqlx::query("SELECT 'Nishant' as name")
-        .fetch_one(&pool)
-        .await?;
+    let book = Book {
+        title: "Atomic Habits".to_string(),
+        author: "James clear".to_string(),
+        isbn: "978-1847941831".to_string(),
+    };
 
-    let name: String = res.get("name");
+    // create(&book, &pool).await?;
+    // let res = sqlx::query("SELECT 'Nishant' as name")
+    //     .fetch_one(&pool)
+    //     .await?;
+
+    // let name: String = res.get("name");
 
     let rows = sqlx::query("SELECT * FROM book").fetch_all(&pool).await?;
 
     println!("{:?}", rows);
-    println!("Name = {}", name);
+    // println!("Name = {}", name);
 
     Ok(())
 }
@@ -40,12 +66,18 @@ async fn main() -> Result<(), Box<dyn Error>> {
 6. Run command at terminal to see logs:
   `cargo run -q`
 
+
+---- Migrations ------
+
 7. We install`cargo install  sqlx-cli` for migrations
 8. Run command for query : ` sqlx migrate add books_table`
 9. Go to sql file and write own migratin script
 10. Add migration at main file , with db connection
 11. two things, if we have already add in main file or we run sqlx run at external
 12. So, after all done and everything fine
+
+
+----Interact with Table -----
 
 13. We are running sql in docker form so we need to go inside to see what is happening
   1. docker ps
@@ -66,7 +98,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     so, we can create a new build script file, for every time we call or run , this script will be executed
     https://docs.rs/sqlx/latest/sqlx/macro.migrate.html
 
-    `sqlx migrate build-script`
+   Run this command  ->  `sqlx migrate build-script`
+   It created build.rs file, it will run for every changes
 
 
+----- SQLx Data writing -----
+   1. Create struct and query the table
 */

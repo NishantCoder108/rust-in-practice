94e0b9feadc6c4d033f1858860c1b32a4b07342c
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-05-15 06:45:02 +0530

    func: recoverable errors, experiment with match and unwrap_or_else
    - we can do nested match expression and unwrap the result value.
    - instead of match expression, we can use unwrap_or_else concept to find
      result value as a result

--- experiment/error-handling-exp/src/main.rs
+++ experiment/error-handling-exp/src/main.rs
@@ -1,22 +1,41 @@
-// fn main() {
-//     panic!("crash and burn");
-// }
-// fn main() {
-//     let v = vec![1, 2, 3];
-
-//     v[99];
-// }
-
-struct CleanupDetector;
-
-impl Drop for CleanupDetector {
-    fn drop(&mut self) {
-        println!("✨ Cleanup: The destructor was called!");
-    }
+use std::{fs::File, io::ErrorKind, panic};
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
 }
 
+// fn main() {
+//     let greeting = File::open("hello.txt");
+
+//     println!("File Greeting: {:?}", greeting);
+
+//     let greeting_file = match greeting {
+//         Ok(file) => file,
+//         Err(err) => match err.kind() {
+//             ErrorKind::NotFound => match File::create("hello.txt") {
+//                 Ok(file) => file,
+//                 Err(err) => panic!("Problem with creating the file : {:?}", err),
+//             },
+
+//             _ => panic!("Problem with opening the file: {:?}", err),
+//         },
+//     };
+
+//     println!("Greeting file : {:?} ", greeting_file);
+// }
+
 fn main() {
-    let _detector = CleanupDetector;
-    println!("About to panic...");
-    panic!("Normal panic occurs!");
+    let greeting = File::open("world.txt");
+
+    let g_file = greeting.unwrap_or_else(|err| {
+        if err.kind() == ErrorKind::NotFound {
+            File::create("world.txt").unwrap_or_else(|err| {
+                panic! {"problem with creating file: {err:?}"}
+            })
+        } else {
+            panic!("Problem with opening the file: {err:?}")
+        }
+    });
+
+    println!("Greetings file : {g_file:?}");
 }

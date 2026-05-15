ce3194bb4eeb358f0b494806e944bcbf442b1264
nishantcoder108 <69218488+NishantCoder108@users.noreply.github.com>
2026-05-15 06:02:34 +0530

    expe: unrecoverable errors with panic

--- experiment/error-handling-exp/src/main.rs
+++ experiment/error-handling-exp/src/main.rs
@@ -1,3 +1,22 @@
+// fn main() {
+//     panic!("crash and burn");
+// }
+// fn main() {
+//     let v = vec![1, 2, 3];
+
+//     v[99];
+// }
+
+struct CleanupDetector;
+
+impl Drop for CleanupDetector {
+    fn drop(&mut self) {
+        println!("✨ Cleanup: The destructor was called!");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _detector = CleanupDetector;
+    println!("About to panic...");
+    panic!("Normal panic occurs!");
 }
